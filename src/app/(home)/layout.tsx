cfebcbb5a8fa69f653b7697ad76a0b1acf8492ad
aok207 <aungookhant007@gmail.com>
2025-04-05 10:41:08 +0630

    remove clerk provider

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import PageLayout from "@/components/page-layout";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,19 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={inter.className} suppressHydrationWarning>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <PageLayout>{children}</PageLayout>
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className} suppressHydrationWarning>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <PageLayout>{children}</PageLayout>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
